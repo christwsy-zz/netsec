4e7a54e23283f18ff7e255380d7e918753bd947b
John Eversole <eversojk@gmail.com>
2014-02-18 14:50:28 -0500

    Added GetIdentification, formatting

--- lab2/MessageParser.java
+++ lab2/MessageParser.java
@@ -5,7 +5,6 @@ import java.net.*;
 import java.security.*;
 import java.math.*;
 
-
 public class MessageParser {
     //Monitor Handling Declarations
     int COMMAND_LIMIT = 25;
@@ -18,7 +17,7 @@ public class MessageParser {
     StringTokenizer t;
     String IDENT = "Skipper";
     String PASSWORD = "franco";
-    static String COOKIE ="193U7UPJPUPRQLUE2RY";
+    static String COOKIE ="default";
     String PPCHECKSUM="";
     int HOST_PORT;
     public static int IsVerified;
@@ -42,7 +41,7 @@ public class MessageParser {
     }
 
     public MessageParser(String ident, String password) {
-        filename = ident+".dat";
+        filename = ident + ".dat";
         PASSWORD = password;
         IDENT = ident;
         GetIdentification(); // Gets Password and Cookie from 'passwd.dat' file
@@ -95,10 +94,10 @@ public class MessageParser {
             while (!(temp.trim().equals(sDefault.trim()))) temp = t.nextToken();
             temp = t.nextToken();
             System.out.println("MessageParser [getNextCommand]: returning:\n\t"+
-            temp);
+                temp);
             return temp;  //returns what the monitor wants
-      } catch (NoSuchElementException e) {  return null;  }
-   }
+        } catch (NoSuchElementException e) {  return null;  }
+    }
 
     public boolean Login() {
         boolean success = false;
@@ -116,232 +115,249 @@ public class MessageParser {
             }
         } catch (NullPointerException n) {
             System.out.println("MessageParser [Login]: null pointer error "+
-                "at login:\n\t"+n);
+            "at login:\n\t"+n);
         }
         return success;
     }
 
-   //Handle Directives and Execute appropriate commands with one argument
-   public boolean Execute (String sentmessage, String arg) {
-      boolean success = false;
-      try {
-	 if (sentmessage.trim().equals("PARTICIPANT_HOST_PORT")) {
-	    sentmessage = sentmessage.concat(" ");
-            sentmessage = sentmessage.concat(arg);
-            SendIt(sentmessage);
+    //Handle Directives and Execute appropriate commands with one argument
+    public boolean Execute (String sentmessage, String arg) {
+        boolean success = false;
+        try {
+            if (sentmessage.trim().equals("PARTICIPANT_HOST_PORT")) {
+                sentmessage = sentmessage.concat(" ");
+                sentmessage = sentmessage.concat(arg);
+                SendIt(sentmessage);
+                success = true;
+            }
+        } catch (IOException e) {
+            System.out.println("IOError:\n\t"+e);
+            success = false;
+        } catch (NullPointerException n) {
+            System.out.println("Null Error has occured");
+            success=false;
+        }
+        return success;
+    }
+
+    //Handle Directives and Execute appropriate commands
+    public boolean Execute (String sentmessage) {
+        boolean success = false;
+        try {
+            if (sentmessage.trim().equals("IDENT")) {
+                sentmessage = sentmessage.concat(" ");
+                sentmessage = sentmessage.concat(IDENT);
+                SendIt (sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("PASSWORD")) {
+                sentmessage = sentmessage.concat(" ");
+                sentmessage = sentmessage.concat(PASSWORD);
+                SendIt (sentmessage.trim());
+                success = true;
+            } else if (sentmessage.trim().equals("HOST_PORT")) {
+                sentmessage = sentmessage.concat(" ");
+                sentmessage = sentmessage.concat(HOSTNAME);//hostname
+                sentmessage = sentmessage.concat(" ");
+                sentmessage = sentmessage.concat(String.valueOf(HOST_PORT));
+                SendIt (sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("ALIVE")) {
+                sentmessage = sentmessage.concat(" ");
+                sentmessage = sentmessage.concat(COOKIE);
+                SendIt (sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("QUIT")) {
+                SendIt(sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("SIGN_OFF")) {
+                SendIt(sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("GET_GAME_IDENTS")) {
+                SendIt(sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("PARTICIPANT_STATUS")) {
+                SendIt(sentmessage);
+                success = true;
+            } else if (sentmessage.trim().equals("RANDOM_PARTICIPANT_HOST_PORT")){
+                SendIt(sentmessage);
+                success = true;
+            }
+        } catch (IOException e) {
+            System.out.println("IOError:\n\t"+e);
+            success = false;
+        } catch (NullPointerException n) {
+            System.out.println("Null Error has occured");
+            success=false;
+        }
+        return success;
+    }
+
+    public void SendIt (String message) throws IOException {
+    try {
+        System.out.println("MessageParser [SendIt]: sent:\n\t"+message);
+        out.println(message);
+        if (out.checkError() == true) throw (new IOException());
+        out.flush();
+        if(out.checkError() == true) throw (new IOException());
+    } catch (IOException e) {} //Bubble the Exception upwards
+    }
+
+    //In future send parameters here so that diff commands are executed
+    public boolean ProcessExtraMessages() {
+        boolean success = false;
+        System.out.println("MessageParser [ExtraCommand]: received:\n\t"+
+        mesg.trim());
+
+        if ((mesg.trim().equals("")) || (mesg.trim().equals(null))) {
+            mesg = GetMonitorMessage();
+            System.out.println("MessageParser [ExtraCommand]: received (2):\n\t"+
+            mesg.trim());
+        }
+
+        String id = GetNextCommand (mesg, "");
+
+        if (id == null) { // No Require, can Launch Free Form Commands Now
+            if (Execute("PARTICIPANT_STATUS")) { //Check for Player Status
+                mesg = GetMonitorMessage();
+                success = true;
+                try {
+                    SaveResources(mesg);  //Save the data to a file
+                    SendIt("SYNTHESIZE WEAPONS");
+                    mesg = GetMonitorMessage();
+                    SendIt("SYNTHESIZE COMPUTERS");
+                    mesg = GetMonitorMessage();
+                    SendIt("SYNTHESIZE VEHICLES");
+                    mesg = GetMonitorMessage();
+                    if (Execute("PARTICIPANT_STATUS")) { //Check for Player Status
+                        mesg = GetMonitorMessage();
+                        success = true;
+                        SaveResources(mesg);//Save the data to a file
+                    }
+                } catch (IOException e) {}
+            }
+        } else {
+            mesg = GetMonitorMessage();
+            System.out.println("MessageParser [ExtraCommand]: failed "+
+            "extra message parse");
+        }
+        return success;
+    }
+
+    public void MakeFreeFlowCommands() throws IOException {
+    }
+
+    public void SaveResources(String res) throws IOException {
+        System.out.println("MessageParser [SaveResources]:");
+        try {  // If an error occurs then don't update the Resources File
+            String temp = GetNextCommand (res, "COMMAND_ERROR");
+            if ((temp == null) || (temp.equals(""))) {
+                fOut = new PrintWriter(new FileWriter(ResourceFileName));
+                t = new StringTokenizer(res," :\n");
+                try {
+                    temp = t.nextToken();
+                    temp = t.nextToken();
+                    temp = t.nextToken();
+                    System.out.println("MessageParser [SaveResources]: got "+
+                    "token before write: "+temp);
+                    for (int i=0 ; i < 20 ; i++) {
+                        fOut.println(temp);
+                        fOut.flush();
+                        temp = t.nextToken();
+                    }
+                } catch (NoSuchElementException ne) {
+                    temp = "";
+                    fOut.close();
+                }
+            }
+            fOut.close();
+        } catch (IOException e) { fOut.close(); }
+    }
+
+    public void HandleTradeResponse(String cmd) throws IOException {
+    }
+
+    public boolean IsTradePossible(String TradeMesg) {
+        return false;
+    }
+
+    public int GetResource(String choice) throws IOException {
+        return 0;
+    }
+
+    public void HandleWarResponse(String cmd) throws IOException{
+    }
+
+    public void DoTrade(String cmd)  throws IOException{
+    }
+
+    public void DoWar(String cmd)  throws IOException{
+    }
+
+    public void ChangePassword(String newpassword) {
+        GetIdentification(); //Gives u the previous values of Cookie and Password
+        String quer = "CHANGE_PASSWORD "+PASSWORD+" "+newpassword;
+        UpdatePassword(quer, newpassword);
+    }
+
+    //Update Password
+    //throws IOException
+    public void UpdatePassword(String cmd, String newpassword) {
+    }
+
+    public void GetIdentification() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line = "";
+            String lastLine = "";
+
+            while ((line = reader.readLine()) != null) {
+                if (lastLine.equals("PASSWORD")) {
+                    this.PASSWORD = line;
+                }
+                else if (lastLine.equals("COOKIE")) {
+                    this.COOKIE = line;
+                }
+                lastLine = line;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File " + filename + " not found, continuing.");
+        } catch (IOException e) {
+            System.out.println("IOException when reading " + filename + " .");
+        }
+    }
+
+    // Write Personal data such as Password and Cookie
+    public boolean  WritePersonalData(String Passwd,String Cookie) {
+        boolean success = false;
+        PrintWriter pout = null;
+        try {
+            if ((Passwd != null) && !(Passwd.equals(""))) {
+                pout = new PrintWriter(new FileWriter(filename));
+                pout.println("PASSWORD");
+                pout.println(Passwd); //(PASSWORD);
+            }
+            if ((Cookie != null) && !(Cookie.equals(""))) {
+                pout.println("COOKIE");
+                pout.flush();
+                pout.println(Cookie);
+                pout.flush();
+            }
+            pout.close();
             success = true;
-         }
-      } catch (IOException e) {
-	 System.out.println("IOError:\n\t"+e);
-	 success = false;
-      } catch (NullPointerException n) {
-	 System.out.println("Null Error has occured");
-	 success=false;
-      }
-      return success;
-   }
+        } catch (IOException e) {
+            pout.close();
+            return success;
+        } catch (NumberFormatException n) {
+        }
+        return success;
+    }
 
-   //Handle Directives and Execute appropriate commands
-   public boolean Execute (String sentmessage) {
-      boolean success = false;
-      try {
-         if (sentmessage.trim().equals("IDENT")) {
-            sentmessage = sentmessage.concat(" ");
-            sentmessage = sentmessage.concat(IDENT);
-            SendIt (sentmessage);
+    //Check whether the Monitor is Authentic
+    public boolean Verify(String passwd,String chksum) {
+        return false;
+    }
 
-            success = true;
-         } else if (sentmessage.trim().equals("PASSWORD")) {
-            sentmessage = sentmessage.concat(" ");
-            sentmessage = sentmessage.concat(PASSWORD);
-            SendIt (sentmessage.trim());
-            success = true;
-         } else if (sentmessage.trim().equals("HOST_PORT")) {
-            sentmessage = sentmessage.concat(" ");
-            sentmessage = sentmessage.concat(HOSTNAME);//hostname
-            sentmessage = sentmessage.concat(" ");
-            sentmessage = sentmessage.concat(String.valueOf(HOST_PORT));
-            SendIt (sentmessage);
-            success = true;
-         } else if (sentmessage.trim().equals("ALIVE")) {
-	    sentmessage = sentmessage.concat(" ");
-            sentmessage = sentmessage.concat(COOKIE);
-            SendIt (sentmessage);
-            success = true;
-         } else if (sentmessage.trim().equals("QUIT")) {
-            SendIt(sentmessage);
-            success = true;
-         } else if (sentmessage.trim().equals("SIGN_OFF")) {
-            SendIt(sentmessage);
-            success = true;
-         } else if (sentmessage.trim().equals("GET_GAME_IDENTS")) {
-            SendIt(sentmessage);
-            success = true;
-         } else if (sentmessage.trim().equals("PARTICIPANT_STATUS")) {
-            SendIt(sentmessage);
-            success = true;
-         } else if (sentmessage.trim().equals("RANDOM_PARTICIPANT_HOST_PORT")){
-            SendIt(sentmessage);
-            success = true;
-         }
-      } catch (IOException e) {
-	 System.out.println("IOError:\n\t"+e);
-	 success = false;
-      } catch (NullPointerException n) {
-	 System.out.println("Null Error has occured");
-	 success=false;
-      }
-      return success;
-   }
-
-   public void SendIt (String message) throws IOException {
-      try {
-	 System.out.println("MessageParser [SendIt]: sent:\n\t"+message);
-         out.println(message);
-         if (out.checkError() == true) throw (new IOException());
-         out.flush();
-         if(out.checkError() == true) throw (new IOException());
-      } catch (IOException e) {} //Bubble the Exception upwards
-   }
-
-   //In future send parameters here so that diff commands are executed
-   public boolean ProcessExtraMessages() {
-      boolean success = false;
-      System.out.println("MessageParser [ExtraCommand]: received:\n\t"+
-			 mesg.trim());
-
-      if ((mesg.trim().equals("")) || (mesg.trim().equals(null))) {
-	 mesg = GetMonitorMessage();
-	 System.out.println("MessageParser [ExtraCommand]: received (2):\n\t"+
-			    mesg.trim());
-      }
-
-      String id = GetNextCommand (mesg, "");
-
-      if (id == null) { // No Require, can Launch Free Form Commands Now
-	 if (Execute("PARTICIPANT_STATUS")) { //Check for Player Status
-	    mesg = GetMonitorMessage();
-	    success = true;
-	    try {
-	       SaveResources(mesg);  //Save the data to a file
-	       SendIt("SYNTHESIZE WEAPONS");
-	       mesg = GetMonitorMessage();
-	       SendIt("SYNTHESIZE COMPUTERS");
-	       mesg = GetMonitorMessage();
-	       SendIt("SYNTHESIZE VEHICLES");
-	       mesg = GetMonitorMessage();
-	       if (Execute("PARTICIPANT_STATUS")) { //Check for Player Status
-		  mesg = GetMonitorMessage();
-		  success = true;
-		  SaveResources(mesg);//Save the data to a file
-	       }
-	    } catch (IOException e) {}
-	 }
-      } else {
-	 mesg = GetMonitorMessage();
-	 System.out.println("MessageParser [ExtraCommand]: failed "+
-			    "extra message parse");
-      }
-      return success;
-   }
-
-   public void MakeFreeFlowCommands() throws IOException {
-   }
-
-   public void SaveResources(String res) throws IOException {
-      System.out.println("MessageParser [SaveResources]:");
-      try {  // If an error occurs then don't update the Resources File
-         String temp = GetNextCommand (res, "COMMAND_ERROR");
-         if ((temp == null) || (temp.equals(""))) {
-            fOut = new PrintWriter(new FileWriter(ResourceFileName));
-            t = new StringTokenizer(res," :\n");
-            try {
-               temp = t.nextToken();
-					temp = t.nextToken();
-               temp = t.nextToken();
-               System.out.println("MessageParser [SaveResources]: got "+
-				  "token before write: "+temp);
-               for (int i=0 ; i < 20 ; i++) {
-                  fOut.println(temp);
-		  fOut.flush();
-                  temp = t.nextToken();
-               }
-            } catch (NoSuchElementException ne) {
-	       temp = "";
-	       fOut.close();
-	    }
-         }
-         fOut.close();
-      } catch (IOException e) { fOut.close(); }
-   }
-
-   public void HandleTradeResponse(String cmd) throws IOException {
-   }
-
-   public boolean IsTradePossible(String TradeMesg) {
-      return false;
-   }
-
-   public int GetResource(String choice) throws IOException {
-      return 0;
-   }
-
-   public void HandleWarResponse(String cmd) throws IOException{
-   }
-
-   public void DoTrade(String cmd)  throws IOException{
-   }
-
-   public void DoWar(String cmd)  throws IOException{
-   }
-
-   public void ChangePassword(String newpassword) {
-      GetIdentification(); //Gives u the previous values of Cookie and Password
-      String quer = "CHANGE_PASSWORD "+PASSWORD+" "+newpassword;
-      UpdatePassword(quer,newpassword);
-   }
-
-   //Update Password
-   //throws IOException
-   public void UpdatePassword(String cmd, String newpassword) {
-   }
-
-   public void GetIdentification() {
-   }
-
-
-   // Write Personal data such as Password and Cookie
-   public boolean  WritePersonalData(String Passwd,String Cookie) {
-      boolean success = false;
-      PrintWriter pout = null;
-      try {
-         if ((Passwd != null) && !(Passwd.equals(""))) {
-            pout = new PrintWriter(new FileWriter(filename));
-            pout.println("PASSWORD");
-            pout.println(Passwd); //(PASSWORD);
-         }
-         if ((Cookie != null) && !(Cookie.equals(""))) {
-            pout.println("COOKIE");
-	    pout.flush();
-            pout.println(Cookie);
-	    pout.flush();
-         }
-         pout.close();
-         success = true;
-      } catch (IOException e) {
-         pout.close();
-	 return success;
-      } catch (NumberFormatException n) {
-      }
-      return success;
-   }
-
-   //Check whether the Monitor is Authentic
-   public boolean Verify(String passwd,String chksum) {
-      return false;
-   }
-
-   public boolean IsMonitorAuthentic(String MonitorMesg) {
-      return false;
-   }
+    public boolean IsMonitorAuthentic(String MonitorMesg) {
+        return false;
+    }
 }
