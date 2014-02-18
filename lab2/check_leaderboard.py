773b76033b52f81c85897f889c51bd015c46ca19
John Eversole <eversojk@gmail.com>
2014-02-18 17:29:47 -0500

    Cleaned up script

--- lab2/check_leaderboard.py
+++ lab2/check_leaderboard.py
@@ -2,32 +2,46 @@
 
 import datetime
 import urllib
+import sys
 
 from bs4 import BeautifulSoup
 
 url = 'http://gauss.ececs.uc.edu/standings8180.html'
-page = urllib.urlopen(url)
-soup = BeautifulSoup(page)
 
-p = soup.findAll('p')[:4]
-updated = p[2].text
-updated = datetime.datetime.strptime(updated, "%a %b %d %H:%M:%S %Z %Y")
-now = datetime.datetime.now()
-diff = now-updated
-print 'Last updated: %s seconds ago' % (diff.total_seconds())
+def fetch_html(url):
+    return urllib.urlopen(url)
 
-tables = soup.findAll('table')
-rows = tables[1].findAll('tr')
-skip = 0
-for r in rows:
-    tds = r.findAll('td', text=True)
+def parse_html(html):
+    return BeautifulSoup(html)
 
-    if not tds:
-        skip += 1
-        continue
+def get_last_update_time(soup):
+    p = soup.findAll('p')[:4]
+    return datetime.datetime.strptime(p[2].text, "%a %b %d %H:%M:%S %Z %Y")
 
-    name = tds[0].text
-    if name == "JOHNE":
-        pts = tds[2].text
-        print 'PTS:', pts
-        break
+def secs_since_update(last_updated):
+    now = datetime.datetime.now()
+    return (now - last_updated).total_seconds()
+
+def find_points(soup, user):
+    tables = soup.findAll('table')
+    rows = tables[1].findAll('tr')
+    for r in rows:
+        tds = r.findAll('td', text=True)
+
+        if not tds:
+            continue
+
+        name = tds[0].text
+        if name.lower() == user.lower():
+            return tds[2].text
+
+def main(args):
+    user = args[0]
+    html = fetch_html(url)
+    soup = parse_html(html)
+    update_time = get_last_update_time(soup)
+    print 'Last updated: %s seconds ago' % (secs_since_update(update_time))
+    print 'PTS:', find_points(soup, user)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
