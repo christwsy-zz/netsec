a03b89bbd2fdffaab58a196c72bdcff8a30ffc1e
John Eversole <eversojk@gmail.com>
2014-02-18 17:34:08 -0500

    More cleanup

--- lab2/check_leaderboard.py
+++ lab2/check_leaderboard.py
@@ -14,32 +14,29 @@ def fetch_html(url):
 def parse_html(html):
     return BeautifulSoup(html)
 
-def get_last_update_time(soup):
-    p = soup.findAll('p')[:4]
-    return datetime.datetime.strptime(p[2].text, "%a %b %d %H:%M:%S %Z %Y")
+def last_update_time(soup):
+    p = soup.findAll('p')[2]
+    return datetime.datetime.strptime(p.text, "%a %b %d %H:%M:%S %Z %Y")
 
 def secs_since_update(last_updated):
-    now = datetime.datetime.now()
-    return (now - last_updated).total_seconds()
+    return (datetime.datetime.now() - last_updated).total_seconds()
 
 def find_points(soup, user):
-    tables = soup.findAll('table')
-    rows = tables[1].findAll('tr')
+    table = soup.findAll('table')[1]
+    rows = table.findAll('tr')
     for r in rows:
         tds = r.findAll('td', text=True)
-
         if not tds:
             continue
-
-        name = tds[0].text
-        if name.lower() == user.lower():
+        # check is desired user
+        if tds[0].text.lower() == user.lower():
             return tds[2].text
 
 def main(args):
     user = args[0]
     html = fetch_html(url)
     soup = parse_html(html)
-    update_time = get_last_update_time(soup)
+    update_time = last_update_time(soup)
     print 'Last updated: %s seconds ago' % (secs_since_update(update_time))
     print '%s\'s points: %s' % (user, find_points(soup, user))
 
