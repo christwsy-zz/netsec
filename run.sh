aa69d5f9de726e8187f9fc64a6f78a1249517b18
John Eversole <eversojk@gmail.com>
2014-02-27 15:59:27 -0500

    Fixed compile order

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-javac *.java && java Homework helios.ececs.uc.edu 8180 johne
+rm *.class
+javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java Homework helios.ececs.uc.edu 8180 johne
