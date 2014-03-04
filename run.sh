7f2320bf8b3a0f2ca5135b835d73e7615ffccfda
John Eversole <eversojk@gmail.com>
2014-03-04 18:01:06 -0500

    Doesn't work at home!

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 rm *.class
 javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java Homework helios.ececs.uc.edu 8180 johne
+#javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java Homework localhost 8180 johne
