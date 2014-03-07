0d1edb38cc6d7c6994f0d4b212696f18d1b77115
John Eversole <eversojk@gmail.com>
2014-03-07 13:46:02 -0500

    Added rounds

--- ZKP.java
+++ ZKP.java
@@ -6,16 +6,26 @@ class ZKP {
     BigInteger n;
     BigInteger v;
     int size;
+    BigInteger[] rounds;
+    SecureRandom sr;
 
-    public ZKP () {
+    public ZKP (int numRounds) {
         size = 1024;
-        SecureRandom sr = new SecureRandom();
+        rounds = new BigInteger[numRounds];
+        sr = new SecureRandom();
         s = new BigInteger(size, sr);
         n = new BigInteger(size, sr);
         v = s.modPow(new BigInteger("2"), n);
     }
 
+    public void doRounds() {
+        for (int i=0; i < rounds.length; i++) {
+            rounds[i] = new BigInteger(size, sr).modPow(new BigInteger("2"), n);
+        }
+    }
+
     public static void main(String[] args) {
-        ZKP a =  new ZKP();
+        ZKP a =  new ZKP(7);
+        a.doRounds();
     }
 }
