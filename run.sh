1eb13e014b31ec8b18083ff2db323ca872799fe0
John Eversole <eversojk@gmail.com>
2014-03-19 14:38:25 -0400

    Added creating of DHKey file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm *.class
-javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java Homework helios.ececs.uc.edu 8180 johne
-#javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java Homework localhost 8180 johne
+#javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java PlantDHKey && java Homework helios.ececs.uc.edu 8180 johne
+javac PlantDHKey.java && javac DiffieHellmanExchange.java && javac Karn.java && javac MessageParser.java && javac ActiveClient.java && javac Server.java && javac Homework.java && java PlantDHKey && java Homework localhost 8180 johne
